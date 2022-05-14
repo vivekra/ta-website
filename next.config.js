782fb49f5e6b6552c85f30be4e9989719f244e91
Vivek Raghunathan <vivekraghunathan@digitalq.co.in>
2022-05-14 22:11:39 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
@@ -11,3 +11,13 @@ module.exports = {
     return config;
   },
 };
+
+*/
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const nextConfiguration = {
+  target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+};
+
+module.exports = withPlugins([optimizedImages], nextConfiguration);
